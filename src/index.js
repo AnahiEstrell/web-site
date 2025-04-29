762913d23cc374c04e04287069051174b054073f
Estefany Anahi Estrella Ortiz <estefany.estrella@gobierno1.yucatan.gob.mx>
2025-04-29 12:01:39 -0600

    src.index: Initial configuration

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+require('dotenv').config();
+const routes = require('./routes/routes');
+const PORT = process.env.PORT || 3000;
+
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+// Views
+app.set('views', path.join(__dirname, 'views'));
+// EJS
+app.set('view engine', 'ejs');
+
+app.use(routes);
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
