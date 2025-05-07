38b2ca2b9cb7febc80e1085b3933752733340d1c
Estefany Anahi Estrella Ortiz <estefany.estrella@gobierno1.yucatan.gob.mx>
2025-05-07 10:07:31 -0600

    index.js: Using json import

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ app.set('views', path.join(__dirname, 'views'));
 // EJS
 app.set('view engine', 'ejs');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 app.use(routes);
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
